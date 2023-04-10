d20ecf7e3615141d93ad0db7c89c5637292aa76f
Gabaquero <gbaquero76@gmail.com>
2023-04-10 01:12:15 -0500

    Robot Utils first implementation

---
+++ static/utils/arduinoControll.ino
@@ -0,0 +1,77 @@
+#include <Servo.h>
+
+#define numOfValsRec 12
+#define digitsPerValRec 3
+
+Servo thumb, index, middle, ring, pinky, wrist, shoulder, arm, eyeUpDown, eyeLeftRight, neck, jaw;
+
+const int servoPins[numOfValsRec] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
+
+int ValsRec[numOfValsRec];
+int stringLength = numOfValsRec * digitsPerValRec + 1; //$000000000000
+int counter = 0;
+bool counterStart = false;
+String receivedString;
+
+void setup() {
+  Serial.begin(9600);
+
+  for (int i = 0; i < numOfValsRec; i++) {
+    Servo servo;
+    servo.attach(servoPins[i]);
+    switch (i) {
+      case 0: thumb = servo; break;
+      case 1: index = servo; break;
+      case 2: middle = servo; break;
+      case 3: ring = servo; break;
+      case 4: pinky = servo; break;
+      case 5: wrist = servo; break;
+      case 6: shoulder = servo; break;
+      case 7: arm = servo; break;
+      case 8: eyeUpDown = servo; break;
+      case 9: eyeLeftRight = servo; break;
+      case 10: neck = servo; break;
+      case 11: jaw = servo; break;
+    }
+  }
+}
+
+void receiveData() {
+  while (Serial.available()) {
+    char c = Serial.read();
+    if (c == '$') {
+      counterStart = true;
+    }
+    if (counterStart) {
+      if (counter < stringLength) {
+        receivedString = String(receivedString + c);
+        counter++;
+      }
+      if (counter >= stringLength) {
+        for (int i = 0; i < numOfValsRec; i++) {
+          int num = (i * digitsPerValRec) + 1;
+          ValsRec[i] = receivedString.substring(num, num + digitsPerValRec).toInt();
+        }
+        receivedString = "";
+        counter = 0;
+        counterStart = false;
+      }
+    }
+  }
+}
+
+void loop() {
+  receiveData();
+  thumb.write(ValsRec[0]);
+  index.write(ValsRec[1]);
+  middle.write(ValsRec[2]);
+  ring.write(ValsRec[3]);
+  pinky.write(ValsRec[4]);
+  wrist.write(ValsRec[5]);
+  shoulder.write(ValsRec[6]);
+  arm.write(ValsRec[7]);
+  eyeUpDown.write(ValsRec[8]);
+  eyeLeftRight.write(ValsRec[9]);
+  neck.write(ValsRec[10]);
+  jaw.write(ValsRec[11]);
+}
--- static/utils/robotUtils.py
+++ static/utils/robotUtils.py
@@ -1,2 +1,57 @@
 import openai
+import serial
+import json
+import os
+from dotenv import load_dotenv
 
+# Load API keys
+load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+# Set up the serial connection with your Arduino
+ser = serial.Serial("COM3", 9600)  # Replace "COM3" with your Arduino's COM port
+
+# Function to send commands to GPT-3.5 Turbo
+def send_gpt3_command(command):
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=f"{command}.\nTranslate the command into servo movements:",
+        max_tokens=50,
+        n=1,
+        stop=None,
+        temperature=0.1,
+    )
+    return response.choices[0].text.strip()
+
+# Function to send servo commands to the Arduino
+def send_servo_command(servo, angle):
+    command = f"{servo}:{angle}"
+    ser.write(command.encode())
+    print(f"Sent command: {command}")
+
+# Main loop
+while True:
+    try:
+        # Get the command from the user
+        user_command = input("Enter a command for the robotic arm: ")
+
+        if user_command.lower() == "exit":
+            break
+
+        # Get servo movements from GPT-3.5 Turbo
+        servo_movements = send_gpt3_command(user_command)
+        print(f"Servo movements: {servo_movements}")
+
+        # Split the servo movements into individual commands
+        movements = servo_movements.split(',')
+
+        # Send servo commands to the Arduino
+        for movement in movements:
+            servo, angle = movement.strip().split(':')
+            send_servo_command(servo, angle)
+
+    except Exception as e:
+        print(f"Error: {e}")
+
+# Close the serial connection
+ser.close()
